1a69ff6046c96fdb24b3b81596c3345de5c684a9
Takahashi Tomohiko <takahashi@tomohiko.io>
2026-04-11 19:32:43 +0900

    if

--- my-first-react-app-01/src/ForItem.jsx
+++ my-first-react-app-01/src/ForItem.jsx
@@ -1,10 +1,29 @@
+import Download from './Download';
+
 export default function ForItem({ book }) {
+  let dd;
+  // downloadプロパティに応じてタグを分岐
+  if (book.download) {
+    dd = <dd>{book.summary}<Download /></dd>;
+  } else {
+    dd = <dd>{book.summary}</dd>;
+  }
+
   return (
     <>
       <dt>
         {book.title} ({book.price}円)
       </dt>
-      <dd>{book.summary}</dd>
+      // ここで埋め込む
+      {dd}
+      // もしくは即時関数でJSX式に直接埋め込んでも良い
+      {(() => {
+        if (book.download) {
+          return <dd>{book.summary}<Download /></dd>
+        } else {
+          return <dd>{book.summary}</dd>
+        }
+      })()}
     </>
   );
 }
