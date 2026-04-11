b24967373acbe09bfbc82e18e3bb03e548d445a3
Takahashi Tomohiko <takahashi@tomohiko.io>
2026-04-11 16:03:26 +0900

    時刻を１秒毎に更新

--- my-first-react-app-01/src/main.jsx
+++ my-first-react-app-01/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+// root.render(
+// 	<StrictMode>
+// 		<App />
+// 	</StrictMode>,
+// );
+
+setInterval(() => {
+  root.render(
+    <p>current time: {(new Date()).toLocaleString()}</p>
+  );
+}, 1000);
