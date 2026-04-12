2d590be86981ee135c5d1f11bdfb8b1204527aec
Takahashi Tomohiko <takahashi@tomohiko.io>
2026-04-12 16:44:23 +0900

    スタイルの継承

--- my-first-react-app-01/src/ModuleBasic.jsx
+++ my-first-react-app-01/src/ModuleBasic.jsx
@@ -2,8 +2,13 @@ import styles from './ModuleBasic.module.css';
 
 export default function ModuleBasic() {
   return (
-    <div className={styles.panel}>
-      <b>React</b> is JavaScript Library for develop frontend.
-    </div>
+    <>
+      <div className={styles.panel}>
+        <b>React</b> is JavaScript Library for develop frontend.
+      </div>
+      <div className={styles.shadowPanel}>
+        <b>React</b> is JavaScript Library for develop frontend.
+      </div>
+    </>
   );
 }
