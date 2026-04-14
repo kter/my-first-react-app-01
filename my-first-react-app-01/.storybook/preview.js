6b2fe744db52cd3402e2730bbdc014dddd788b29
Takahashi Tomohiko <takahashi@tomohiko.io>
2026-04-14 21:17:56 +0900

    Storyの背景色を変更

--- my-first-react-app-01/.storybook/preview.js
+++ my-first-react-app-01/.storybook/preview.js
@@ -7,6 +7,13 @@ const preview = {
        date: /Date$/i,
       },
     },
+    backgrounds: {
+      options: {
+        ghostwhite: { name: 'Ghostwhite', value: '#f8f8ff' },
+        aquamarine: { name: 'Aquamarine', value: '#7fffd4' },
+        coral: { name: 'Coral', value: '#ff7f50' },
+      }
+    },
 
     a11y: {
       // 'todo' - show a11y violations in the test UI only
