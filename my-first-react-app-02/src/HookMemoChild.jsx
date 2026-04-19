347bd7ff69473e7f0be009d9c752f01fc81ca60f
Takahashi Tomohiko <takahashi@tomohiko.io>
2026-04-19 13:44:19 +0900

    memo関数を使い、コンポーネントの再描画を抑制

--- my-first-react-app-02/src/HookMemoChild.jsx
+++ my-first-react-app-02/src/HookMemoChild.jsx
@@ -1,14 +1,17 @@
-export const MyButton = ({ id, handleClick, children }) => {
+import { memo } from 'react';
+
+export const MyButton = memo(({ id, handleClick, children }) => {
   console.log(`MyButton is called: ${id}`);
   return (
     <button onClick={handleClick}>{children}</button>
   );
-};
+});
 
-export const MyCounter = ({ id, value }) => {
+export const MyCounter = memo(({ id, value }) => {
   console.log(`MyCounter is called: ${id}`);
   return (
     <p>Current: {value}</p>
   );
-}
+});
+
 
