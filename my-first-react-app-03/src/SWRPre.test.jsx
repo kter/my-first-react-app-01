8ae8803e4ecf8dd35cae86bc658e04b7dafca155
Takahashi Tomohiko <takahashi@tomohiko.io>
2026-04-24 09:54:22 +0900

    非同期通信のテスト

---
+++ my-first-react-app-03/src/SWRPre.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import SWRPre from '../../my-first-react-app-01/src/SWRPre';
+
+describe('SWRPre', () => {
+  test('fetch test', async () => {
+    // render(<SWRPre />);
+    // const img = await screen.findByRole('img', {}, { timeout: 3000 });
+    // expect(img).toHaveAttribute('alt', 'Clouds');
+  });
+});
+
